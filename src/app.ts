9c776f26e0af39ee1e345373c3ee8a645a389901
A.K.M Fozlol Hoq <akmfozlolhoq@gmail.com>
2023-06-01 20:36:18 +0600

    created auth service

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
+// import bodyParser from 'body-parser'
+import usersRouter from './app/modules/users/users.route'
 const app: Application = express()
 
 app.use(cors())
 //parser
+// app.use(bodyParser.json())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application routes
+app.use('/api/v1/users', usersRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('Hello world')
 })
 
