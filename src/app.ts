dbcc29ce017c16dd552b68c26360618134bac2b1
A.K.M Fozlol Hoq <akmfozlolhoq@gmail.com>
2023-06-04 23:18:01 +0600

    error is not saving at file

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-import express, { Application, Request, Response } from 'express'
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import cors from 'cors'
+import express, { Application, NextFunction, Request, Response } from 'express'
 // import bodyParser from 'body-parser'
-import usersRouter from './app/modules/users/users.route'
+// import { UserRoutes } from './app/modules/users/user.route'
+import { UserRoutes } from './app/modules/users/user.route'
+import globalErrorHandler from './middlewares/globalErrorHanler'
 const app: Application = express()
 
 app.use(cors())
@@ -11,10 +15,15 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // Application routes
-app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/users', UserRoutes)
 
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Hello world')
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  // res.send('Hello world')
+  // Promise.reject(new Error('Unhandled promies rejection'))
+  throw new Error('Unhandled promies rejection')
 })
+
+//global error hander
+app.use(globalErrorHandler)
 
 export default app
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,49 @@
+/* eslint-disable no-unused-vars */
+import { Server } from 'http'
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import { errorLogger, logger } from './shared/logger'
 
-async function main() {
+let server: Server
+
+process.on('uncaughtException', err => {
+  console.log('Uncaught exception is detected...')
+  // errorLogger.error(err)
+  errorLogger.error('Uncaught exception is detected.')
+  process.exit(1)
+})
+// console.log(sdfh)
+process.on('SIGTERM', () => {
+  console.log('SIGTERM is received')
+  if (server) {
+    server.close()
+  }
+})
+
+async function bootstrap() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Database connection established')
+    logger.info('Database connection established')
 
-    app.listen(config.port, () => {
-      console.log('app is listening on port ' + config.port)
+    server = app.listen(config.port, () => {
+      logger.info('app is listening on port ' + config.port)
     })
   } catch (err) {
-    console.log('Failed to connect to MongoDB')
+    errorLogger.error('Failed to connect to MongoDB')
   }
+
+  process.on('unhandledRejection', error => {
+    console.log('Unhandled rejection, we are closing out server', error)
+    if (server) {
+      server.close(() => {
+        errorLogger.error(error)
+        process.exit(1)
+      })
+    } else {
+      process.exit(1)
+    }
+  })
 }
 
-main()
+bootstrap()
